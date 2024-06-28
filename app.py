a7ff30cddd89a7916f59a55c7c128694a5869300
Christley Olubela <olubelachristley@gmail.com>
2024-06-28 03:57:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def get_conversational_chain():
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=False)
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
